fc9e1162818390fe3d003da72840d5013da41dd6
Matt Rozak <mattjrozak@gmail.com>
2015-03-03 14:53:17 -0500

    THE SINGULARITY

--- facespace/backend/models.py
+++ facespace/backend/models.py
@@ -2,18 +2,23 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+class Comment(models.Model):
+	user_id = models.ForeignKey('FaceSpaceUser')
+	entity_id = models.ForeignKey('Entity')
+	time_created = models.DateTimeField(auto_now_add=True)
+	text = models.TextField()
+
+
+class Entity(models.Model):
+	time_created = models.DateTimeField(auto_now_add=True)
+
+
 class FaceSpaceUser(AbstractUser):
     birthday = models.DateField()
     is_male = models.BooleanField(default=True)
 
 
-class Entities(models.Model):
-	time_created = models.DateTimeField()
-
-
-class Likes(models.Model):
-	pass
-
-
-class Comments(models.Model):
-	pass
+class Like(models.Model):
+	user_id = models.ForeignKey('FaceSpaceUser')
+	entity_id = models.ForeignKey('Entity')
+	is_positive = models.BooleanField()
