8cd88eaa729405c69d204f5dac8f67bcab8578f8
dwj300 <dwj300@gmail.com>
2015-03-05 15:49:35 -0500

    using mysql, and normalizing the model

--- facespace/backend/models.py
+++ facespace/backend/models.py
@@ -5,45 +5,60 @@ class Interest(models.Model):
     name = models.CharField(max_length=50)
     parent = models.ForeignKey('self', related_name="children", null=True)
 
+    class Meta:
+        db_table = 'interests'
+
 
 class FaceSpaceUser(AbstractUser):
     birthday = models.DateField()
     is_male = models.BooleanField(default=True)
     profile_picture = models.ForeignKey('Photo', null=True)
-
     relationship_with = models.ManyToManyField("self", through='Romance', symmetrical=False, related_name='relationship')
     friends_with = models.ManyToManyField("self", through='Friendship', symmetrical=False, related_name='friend')
     interested_in = models.ManyToManyField('Interest')
 
     REQUIRED_FIELDS = ['email', 'first_name', 'last_name', 'birthday', 'is_male']
 
+    class Meta:
+        db_table = 'facespaceusers'
+
 
 class Entity(models.Model):
     time_created = models.DateTimeField(auto_now_add=True)
-    user_id = models.ForeignKey('FaceSpaceUser')
+    user = models.ForeignKey('FaceSpaceUser')
 
     class Meta:
         verbose_name_plural = 'Entities'
+        db_table = 'entities'
 
 
 class Ad(models.Model):
     content_link = models.URLField()
     owner = models.ForeignKey('FaceSpaceUser', null=False)
 
+    class Meta:
+        db_table = 'ads'
+
 
 class AdSlot(models.Model):
     bid_time = models.DateTimeField()
     bid_price = models.DecimalField(max_digits=9, decimal_places=2)
     interest = models.OneToOneField('Interest')
-    holds = models.ForeignKey('Ad', related_name="holding_ad_slots")
-    will_hold = models.ForeignKey('Ad', related_name="will_hold_ad_slots")
+    holds = models.ForeignKey('Ad', related_name='holding_ad_slots')
+    will_hold = models.ForeignKey('Ad', related_name='will_hold_ad_slots', null=True)
+
+    class Meta:
+        db_table = 'adslots'
 
 
 class Comment(models.Model):
-    user_id = models.ForeignKey('FaceSpaceUser', null=False)
-    entity_id = models.ForeignKey('Entity')
+    user = models.ForeignKey('FaceSpaceUser', null=False)
+    entity = models.ForeignKey('Entity')
     time_created = models.DateTimeField(auto_now_add=True)
     text = models.TextField()
+
+    class Meta:
+        db_table = 'comments'
 
 
 class Friendship(models.Model):
@@ -53,20 +68,25 @@ class Friendship(models.Model):
 
     class Meta:
         unique_together = ('from_friend', 'to_friend')
+        db_table = 'friendships'
 
 
 class Like(models.Model):
-    user_id = models.ForeignKey('FaceSpaceUser')
-    entity_id = models.ForeignKey('Entity')
+    user = models.ForeignKey('FaceSpaceUser')
+    entity = models.ForeignKey('Entity')
     is_positive = models.BooleanField(default=True)
 
     class Meta:
-        unique_together = ('user_id', 'entity_id')
+        unique_together = ('user', 'entity')
+        db_table = 'likes'
 
 
 class Photo(Entity):
     caption = models.TextField()
     file_name = models.CharField(max_length=75)
+
+    class Meta:
+        db_table = 'photos'
 
 
 class Romance(models.Model):
@@ -82,10 +102,15 @@ class Romance(models.Model):
     to_partner = models.ForeignKey('FaceSpaceUser', related_name='to_partner')
     romance_type = models.CharField(max_length=10, choices = ROMANCE_TYPES, default=DATING)
     since = models.DateField()
+    until = models.DateField(null=True)
 
     class Meta:
-        unique_together = ('from_partner', 'to_partner')
+        db_table = 'romances'
+        unique_together = ('from_partner', 'to_partner', 'since')
 
 
 class Status(Entity):
     text = models.TextField()
+
+    class Meta:
+        db_table = 'statuses'
--- facespace/facespace/settings.py
+++ facespace/facespace/settings.py
@@ -65,8 +65,11 @@ WSGI_APPLICATION = 'facespace.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'facespace',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
     }
 }
 
