61faf895cb0c488300597efd25329e6ee5ee08d4
Sam Galambos <hamster_pi@msn.com>
2015-03-19 16:09:17 -0400

    add my own settings file

---
+++ facespace/sam_settings.py
@@ -0,0 +1,11 @@
+from .settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'facespace',
+        'USER': 'facespace',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+    }
+}
