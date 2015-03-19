a0250da5e5652206ed969a7ed06faf52b43b8609
Sam Galambos <hamster_pi@msn.com>
2015-03-19 15:56:49 -0400

    remove tabs

--- frontend/views.py
+++ frontend/views.py
@@ -3,20 +3,24 @@ from stronghold.decorators import public
 from backend.models import FaceSpaceUser
 
 # Create your views here.
+
+
 @public
 def index(request):
     return render(request, 'index.html')
 
+
 def home(request):
-	return render(request, 'home.html')
+    return render(request, 'home.html')
+
 
 def profile(request, username):
-	params = {}
-	try:
-		user = FaceSpaceUser.objects.get(username=username)
-		params['facespaceuser'] = user
-	except:
-		# redirect to homepage
-		pass
+    params = {}
+    try:
+        user = FaceSpaceUser.objects.get(username=username)
+        params['facespaceuser'] = user
+    except:
+        # redirect to homepage
+        pass
 
-	return render(request, 'profile.html', params)
+    return render(request, 'profile.html', params)
