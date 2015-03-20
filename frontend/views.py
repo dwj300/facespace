aa597807a1da9b3031402415b1e8bfc2421c2b30
Sam Galambos <hamster_pi@msn.com>
2015-03-20 13:42:31 -0400

    basic interests

--- frontend/views.py
+++ frontend/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from stronghold.decorators import public
-from backend.models import FaceSpaceUser
+from backend.models import FaceSpaceUser, Interest
 
 # Create your views here.
 
@@ -24,3 +24,15 @@ def profile(request, username):
         pass
 
     return render(request, 'profile.html', params)
+
+def interest(request, interest_id):
+    params = {}
+    try:
+        interest = Interest.objects.get(id=interest_id)
+        params['interest'] = interest
+        child_interests = Interest.objects.filter(parent__id=interest_id)
+        params['child_interests'] = child_interests
+    except:
+        pass
+
+    return render(request, 'interest.html', params)
