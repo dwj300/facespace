2b2a261250a0098ba4da124a54386f9edf079cc2
dwj300 <dwj300@gmail.com>
2015-03-26 15:03:54 -0400

    Fixing import

--- backend/views.py
+++ backend/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect  # , render
-from backend.models import FaceSpaceUser, Friendship
+from backend.models import FaceSpaceUser, Friendship, Photo
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
